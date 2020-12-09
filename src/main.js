bcb550f2b533450bf57ba718fb6879918e4587e5
ecruz17 <ecruz17@ucol.mx>
2020-12-09 15:48:45 -0600

    implemented first function

--- src/main.js
+++ src/main.js
@@ -1,3 +1,18 @@
-export default class App {}
+export default class App {
+
+    factorial(numero) {
+
+        let factorial = 1;
+
+        for(let i = 1; i <= numero;i++) {
+
+            factorial = factorial * i;
+        }
+        return factorial;
+    }
+
+}
 
 let app = new App();
+
+console.log(app.factorial(5));
