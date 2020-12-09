1c38649a491c2aa1150ec2ddc8599dbabf2f4464
ecruz17 <ecruz17@ucol.mx>
2020-12-09 15:58:50 -0600

    implemented convertToString function and tested

--- src/main.js
+++ src/main.js
@@ -11,8 +11,25 @@ export default class App {
         return factorial;
     }
 
+    convertirAString(numero) {
+        
+        let asteriscos = "";
+        let count = 1;
+
+        while(count <= numero) {
+
+            asteriscos = `${asteriscos}*`;
+            count++;
+        }
+
+        return asteriscos;
+
+    }
+
 }
 
 let app = new App();
 
 console.log(app.factorial(5));
+
+console.log(app.convertirAString(5));
